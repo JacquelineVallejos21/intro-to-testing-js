5eed91d37526dc6e9257d89c42f20c72a04d8c23
Jacqueline Vallejos <84371982+JacquelineVallejos21@users.noreply.github.com>
2021-06-02 22:23:35 -0500

    Completed Intro to Testing - JS

--- code.js
+++ code.js
@@ -2,16 +2,21 @@
 function helloWorld (){
     return "Hello, World!";
 }
-
+//function sayHello(name) {
+//    if (name === "Jane") {
+//        return 'Hello, Jane!';
+//    }
+//    if (name === "Alex") {
+//        return 'Hello, Alex!';
+//    }
+//    if (name === "Pat") {
+//        return 'Hello, Pat!';
+//   }
+//    return "";
+//}
 function sayHello(name) {
-    if (name === "Jane") {
-        return 'Hello, Jane!';
+    if (typeof(name) != "string") {
+       return "Hello, World!";
     }
-    if (name === "Alex") {
-        return 'Hello, Alex!';
-    }
-    if (name === "Pat") {
-        return 'Hello, Pat!';
-    }
-    return "";
+    return "Hello," + name + "!";
 }
--- tests.js
+++ tests.js
@@ -22,12 +22,21 @@ describe('sayHello', function() {
     expect(typeof sayHello()).toBe("string");
     });
     it('should return the string "Hello, Jane!" when sayHello("Jane") is executed', function() {
-        expect(sayHello("Jane")).toBe("Hello, Jane!");
+        expect(sayHello("Jane")).toBe("Hello,Jane!");
     });
     it('should return the string "Hello, Alex!" when sayHello("Alex") is executed', function() {
-        expect(sayHello("Alex")).toBe("Hello, Alex!");
+        expect(sayHello("Alex")).toBe("Hello,Alex!");
     });
     it('should return the string "Hello, Pat!" when sayHello("Pat") is executed', function() {
-        expect(sayHello("Pat")).toBe("Hello, Pat!");
+        expect(sayHello("Pat")).toBe("Hello,Pat!");
+    });
+    it('should return the string "Hello, World!" when sayHello() is executed', function() {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when sayHello(true) is executed', function() {
+        expect(sayHello(true)).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when sayHello(true) is executed', function() {
+        expect(sayHello(1)).toBe("Hello, World!");
     });
 });
